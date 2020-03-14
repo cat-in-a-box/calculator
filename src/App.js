5e6c06c66ff391e952577d9b940190aad43c4690
cat-in-a-box <timoshuk.alexander@gmail.com>
2020-03-14 16:48:41 +0200

    Removed Helmet package

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 // Установил и импортировал Helmet, с помощью которого изменил page title.
 // Насколько я понял, это самый простой способ менять его (как и атрибуты страницы) на реакте.
 // А еще подгружает скрипты. Нормально.
-import { Helmet } from "react-helmet";
 import Header from "./Components/Header.jsx";
 import Calculator from "./Components/Calculator.jsx";
 
@@ -11,10 +10,6 @@ import Calculator from "./Components/Calculator.jsx";
 function App() {
     return (
         <div className="App">
-            <Helmet>
-                <title>Gallery</title>
-                <meta charSet="utf-8" />
-            </Helmet>
             <Header/>
             <Calculator/>
         </div>
